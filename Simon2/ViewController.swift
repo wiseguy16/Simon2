270afa2daab8dd7f5291b2bce2735d32d77595a2
wiseguy16 <gergusa04@gmail.com>
2016-08-20 18:06:28 -0400

    Storyboard set up

--- Simon2/ViewController.swift
+++ Simon2/ViewController.swift
@@ -8,16 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    
+    @IBOutlet weak var gameStatusLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func startGameTapped(_ sender: UIButton)
+    {
+        
+    }
+    
+    @IBAction func yellowTapped(_ sender: UIButton)
+    {
+        
+    }
+
+    @IBAction func blueTapped(_ sender: UIButton)
+    {
+        
+    }
+    
+    @IBAction func redTapped(_ sender: UIButton)
+    {
+        
+    }
+
+    
+    @IBAction func greenTapped(_ sender: UIButton)
+    {
+        
     }
 
 
